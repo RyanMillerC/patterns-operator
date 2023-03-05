580b126ed2c165182a6fff0296fc1f54298d359f
Ryan Miller <miller@redhat.com>
2023-03-05 00:09:25 -0500

    Add type and controller for PatternCatalogSource
    Command used to generate:
    
    operator-sdk create api --version=v1alpha1 --group=gitops --kind=PatternCatalogSource --resource --controller

--- main.go
+++ main.go
@@ -91,6 +91,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Pattern")
 		os.Exit(1)
 	}
+	if err = (&controllers.PatternCatalogSourceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "PatternCatalogSource")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
