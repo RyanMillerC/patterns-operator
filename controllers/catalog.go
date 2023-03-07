b901209ee62ff9fa472b634c6d5af5e71674d1de
Ryan Miller <miller@redhat.com>
2023-03-06 23:28:12 -0500

    Implement and test getCatalogYAML

---
+++ controllers/catalog.go
@@ -0,0 +1,84 @@
+/*
+
+# What does catalog.go do?
+
+On new PatternCatalogSource...
+
+* Check for URL to YAML source in spec; If not found, error
+* Get YAML from URL
+* Convert YAML to native type
+* For each pattern identified in catalog YAML
+	* Check if PatternManifest exists
+    * Create PatternManifest
+* Check for PatternManifests that exist which are not present in Catalog YAML
+* Schedule process to repeat on interval
+
+*/
+
+package controllers
+
+import (
+	"io"
+	"net/http"
+
+	"gopkg.in/yaml.v3"
+
+	api "github.com/hybrid-cloud-patterns/patterns-operator/api/v1alpha1"
+)
+
+// Generated using https://zhwt.github.io/yaml-to-go/
+type PatternCatalog struct {
+	Version      string       `yaml:"version"`
+	Organization Organization `yaml:"organization"`
+	Patterns     []Patterns   `yaml:"patterns"`
+}
+type Organization struct {
+	Name        string `yaml:"name"`
+	OrgURL      string `yaml:"org-url"`
+	Description string `yaml:"description"`
+	Maintainers string `yaml:"maintainers"`
+}
+type Products struct {
+	Name string `yaml:"name"`
+}
+type Patterns struct {
+	Name        string     `yaml:"name"`
+	Description string     `yaml:"description"`
+	PatternURL  string     `yaml:"pattern-url"`
+	Products    []Products `yaml:"products"`
+	Branch      string     `yaml:"branch"`
+	Maintainers string     `yaml:"maintainers"`
+}
+
+// Given a URL to a catalog.yaml file, fetch that file, and return a object of
+// type CatalogYAML.
+func getCatalogYAML(url string) (*PatternCatalog, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close() // Wait for response
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var catalog PatternCatalog
+	if err := yaml.Unmarshal(body, &catalog); err != nil {
+		return nil, err
+	}
+
+	return &catalog, nil
+}
+
+// Called by the PatternCatalogSource reconciler on each catalog source action
+func processCatalogSource(pcs *api.PatternCatalogSource) error {
+	// TODO: Implement
+	return nil
+}
+
+func createCatalogManifest(notSureYet string) error {
+	// TODO: Implement
+	return nil
+}
---
+++ controllers/catalog_test.go
@@ -0,0 +1,58 @@
+package controllers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetCatalogYAML(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		// The whitespace is important on the following lines. This YAML block
+		// uses leading space characters, NOT tabs like the rest of this file.
+		w.Write([]byte(`version: alpha
+organization:
+  name: Hybrid Cloud Patterns
+  org-url: https://github.com/hybrid-cloud-patterns
+  description: |
+    Open source repository of cloud native solution patterns.
+    This repository is maintained by Red Hat with contributions from customers and partners.
+  maintainers: team-validated-patterns@redhat.com
+patterns:
+  - name: Multi-cloud GitOps
+    description: |
+       This pattern demonstrates a simple architecture for managing multiple clusters using GitOps.
+       It can be used as a good starting point for experimenting with other products in a multi-cluster architecture.
+    pattern-url: https://github.com/hybrid-cloud-patterns/multicloud-gitops
+    products:
+  - name: Red Hat Advanced Cluster Management
+    branch: main
+    maintainers: team-validated-patterns@redhat.com
+  - name: Multicluster DevSecOps
+    description: |
+        This pattern demonstrates a horizontal solution for multicluster DevSecOps use cases.
+        It is derived from the multi-cloud GitOps pattern with added products to provide a complete DevSecOps workflow.
+        This includes CI/CD pipelines with security gates; image scanning, signing and storage in a secure registry; deployment
+        to secured clusters that provide advanced security monitoring and alerting.
+    pattern-url: https://github.com/hybrid-cloud-patterns/multicluster-devsecops
+    products:
+        - name: Red Hat Advanced Cluster Management
+        - name: Red Hat OpenShift GitOps
+    branch: main
+    maintainers: team-validated-patterns@redhat.com`))
+	}))
+	defer server.Close()
+
+	catalog, _ := getCatalogYAML(server.URL)
+	// Check some random data. Probably not worth it to check everything
+	if catalog.Version != "alpha" {
+		t.Errorf("Expected catalog.Version to be 'alpha', got %s", catalog.Version)
+	}
+	if catalog.Organization.Name != "Hybrid Cloud Patterns" {
+		t.Errorf("Expected catalog.Organization.Name to be 'Hybrid Cloud Patterns', got %s", catalog.Organization.Name)
+	}
+	if len(catalog.Patterns) != 3 {
+		t.Errorf("Expected length of catalog.patterns to be 3, got %d", len(catalog.Patterns))
+	}
+}
