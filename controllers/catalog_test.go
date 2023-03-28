33b5bd0c9fd836a7f64b5bcbc4f1e6a1f4918733
Ryan Miller <miller@redhat.com>
2023-03-28 14:51:45 -0400

    Update catalog_test to pass with new catalog.yaml format

--- controllers/catalog_test.go
+++ controllers/catalog_test.go
@@ -14,33 +14,31 @@ func TestGetPatternCatalogYAML(t *testing.T) {
 		w.Write([]byte(`version: alpha
 organization:
   name: Hybrid Cloud Patterns
-  org-url: https://github.com/hybrid-cloud-patterns
-  description: |
-    Open source repository of cloud native solution patterns.
-    This repository is maintained by Red Hat with contributions from customers and partners.
-  maintainers: team-validated-patterns@redhat.com
+  description: >
+    Open source repository of cloud native solution patterns. This repository
+    is maintained by Red Hat with contributions from customers and partners.
+  maintainers:
+    - name: Validated Patterns Team
+      email: team-validated-patterns@redhat.com
+  url: https://github.com/hybrid-cloud-patterns
 patterns:
   - name: Multi-cloud GitOps
-    description: |
-       This pattern demonstrates a simple architecture for managing multiple clusters using GitOps.
-       It can be used as a good starting point for experimenting with other products in a multi-cluster architecture.
-    pattern-url: https://github.com/hybrid-cloud-patterns/multicloud-gitops
-    products:
-  - name: Red Hat Advanced Cluster Management
+    description: A simple architecture for managing multiple clusters using GitOps
+    longDescription: >
+      This pattern demonstrates a simple architecture for managing multiple
+      clusters using GitOps. It can be used as a good starting point for
+      experimenting with other products in a multi-cluster architecture.
     branch: main
-    maintainers: team-validated-patterns@redhat.com
-  - name: Multicluster DevSecOps
-    description: |
-        This pattern demonstrates a horizontal solution for multicluster DevSecOps use cases.
-        It is derived from the multi-cloud GitOps pattern with added products to provide a complete DevSecOps workflow.
-        This includes CI/CD pipelines with security gates; image scanning, signing and storage in a secure registry; deployment
-        to secured clusters that provide advanced security monitoring and alerting.
-    pattern-url: https://github.com/hybrid-cloud-patterns/multicluster-devsecops
+    gitRepo: https://github.com/hybrid-cloud-patterns/multicloud-gitops.git
+    maintainers:
+      - name: Validated Patterns Team
+        email: team-validated-patterns@redhat.com
     products:
-        - name: Red Hat Advanced Cluster Management
-        - name: Red Hat OpenShift GitOps
-    branch: main
-    maintainers: team-validated-patterns@redhat.com`))
+      - Red Hat Advanced Cluster Management
+      - Red Hat OpenShift GitOps
+      - Hashicorp Vault
+    type: Validated
+    url: https://hybrid-cloud-patterns.io/patterns/multicloud-gitops/`))
 	}))
 	defer server.Close()
 
@@ -57,8 +55,8 @@ patterns:
 	if catalog.Organization.Name != "Hybrid Cloud Patterns" {
 		t.Errorf("Expected catalog.Organization.Name to be 'Hybrid Cloud Patterns', got %s", catalog.Organization.Name)
 	}
-	if len(catalog.Patterns) != 3 {
-		t.Errorf("Expected length of catalog.patterns to be 3, got %d", len(catalog.Patterns))
+	if len(catalog.Patterns) != 1 {
+		t.Errorf("Expected length of catalog.patterns to be 1, got %d", len(catalog.Patterns))
 	}
 }
 
