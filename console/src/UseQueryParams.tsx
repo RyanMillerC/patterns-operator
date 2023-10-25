da614c714c22855a67031f98bb9c836487a6623d
Ryan Miller <miller@redhat.com>
2023-10-25 12:04:40 -0400

    Add UseQueryParams for getting/settings query string

---
+++ console/src/UseQueryParams.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+
+interface QueryParamsContextValue {
+  queryParams: Record<string, any>;
+  setQueryParam: (key: string, value: any) => void;
+}
+
+const QueryParamsContext = React.createContext<QueryParamsContextValue | undefined>(
+  undefined
+);
+
+const getParamsFromURL = (): Record<string, any> => {
+  const url = new URL(window.location.href);
+  return Object.fromEntries(url.searchParams.entries());
+};
+
+/* Wrap main component in this to provide context */
+export const QueryParamsProvider: React.FC<React.PropsWithChildren<any>> = (props) => {
+  const [queryParams, setQueryParams] = React.useState<Record<string, any>>(getParamsFromURL);
+
+  const setQueryParam = React.useCallback(
+    (key: string, value: any) => {
+      setQueryParams((p) => {
+        const newParams = { ...p };
+        newParams[key] = value;
+        return newParams;
+      });
+    },
+    [setQueryParams]
+  );
+
+  // Update query params in URL
+  React.useEffect(() => {
+    const url = new URL(window.location.href);
+    url.search = new URLSearchParams(queryParams).toString();
+    window.history.pushState({}, "", url.toString());
+  }, [queryParams]);
+
+  const value = {
+    queryParams,
+    setQueryParam,
+  };
+
+  return (
+    <QueryParamsContext.Provider value={value}>
+      {props.children}
+    </QueryParamsContext.Provider>
+  );
+};
+
+export const useQueryParams = (): QueryParamsContextValue => {
+  const context = React.useContext(QueryParamsContext);
+  if (context === undefined) {
+    throw new Error("You must wrap this component in a QueryParamsProvider");
+  }
+  return context;
+};
--- console/src/catalog/index.tsx
+++ console/src/catalog/index.tsx
@@ -3,40 +3,42 @@ import Helmet from 'react-helmet';
 import { Page, PageSection, TextContent, Text, Title } from '@patternfly/react-core';
 import Catalog from './Catalog';
 import '../main.css';
+import { QueryParamsProvider } from '../UseQueryParams';
 
 const CatalogPage = () => {
-  // https://www.patternfly.org/v4/extensions/catalog-view/catalog-tile
   return (
-    <>
-      <Helmet>
-        <title data-test="example-page-title">Patterns Catalog</title>
-      </Helmet>
-      <Page>
-        <PageSection variant="light">
-          <Title headingLevel="h1">Patterns Catalog</Title>
-        </PageSection>
-        <PageSection variant="light">
-          <TextContent>
-            <Text component="p">
-              <a
-                href="https://hybrid-cloud-patterns.io/"
-                rel="noopener"
-                target="_blank"
-              >
-                Hybrid Cloud Patterns{' '} {/* Force space between words */}
-              </a>
-              are an evolution of how you deploy applications in
-              a hybrid cloud. With a pattern, you can automatically deploy a
-              full application stack through a GitOps-based framework. With this
-              framework, you can create business-centric solutions while
-              maintaining a level of Continuous Integration (CI) over your
-              application.
-            </Text>
-          </TextContent>
-        </PageSection>
-        <Catalog />
-      </Page>
-    </>
+    <div> {/* TODO: div probably isn't needed */}
+      <QueryParamsProvider>
+        <Helmet>
+          <title data-test="example-page-title">Patterns Catalog</title>
+        </Helmet>
+        <Page>
+          <PageSection variant="light">
+            <Title headingLevel="h1">Patterns Catalog</Title>
+          </PageSection>
+          <PageSection variant="light">
+            <TextContent>
+              <Text component="p">
+                <a
+                  href="https://hybrid-cloud-patterns.io/"
+                  rel="noopener"
+                  target="_blank"
+                >
+                  Hybrid Cloud Patterns{' '} {/* Force space between words */}
+                </a>
+                are an evolution of how you deploy applications in
+                a hybrid cloud. With a pattern, you can automatically deploy a
+                full application stack through a GitOps-based framework. With this
+                framework, you can create business-centric solutions while
+                maintaining a level of Continuous Integration (CI) over your
+                application.
+              </Text>
+            </TextContent>
+          </PageSection>
+          <Catalog />
+        </Page>
+      </QueryParamsProvider>
+    </div>
   );
 }
 
