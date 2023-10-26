051eb37b95e36d15b3f007ee56738012481ef036
Ryan Miller <miller@redhat.com>
2023-10-26 09:43:43 -0400

    Document UseQueryParams.tsx

--- console/src/UseQueryParams.tsx
+++ console/src/UseQueryParams.tsx
@@ -1,3 +1,58 @@
+/*
+
+This module provides the capability to get and set query (search)
+string parameters. Query parameters in this application act as a
+simple state management solution for tracking things like a searched
+term, an active modal, etc.
+
+Because components from this application are loaded into the OpenShift
+Console's React UI, normal React single-page app routing does not work
+as expected. The OpenShift console is running react-router-dom. If you
+try use react-router-dom in an OpenShift console dynamic plugin, it
+will not route correctly. (This is Ryan's experience with it as of
+2023-10-26.)
+
+In order to get and set query parameters in the OpenShift console
+dynamic plugin, I'm using standard JS capabilities to read and set
+the URL in the browser address bar. To refresh components when new
+parameters are set, React Context is used. A copy of parameters from
+the browser URL are stored a context state. On a hard refresh, the
+state is loaded from the URL. On each parameter update, both the
+context state and URL are updated.
+
+Example usage:
+
+```
+import { useQueryParams } from '../UseQueryParams';
+
+const MyComponent = () => {
+  // Get and set query parameters in the URL
+  const { queryParams, setQueryParam } = useQueryParams();
+
+  setQueryParam('someParam', 'value');
+  console.log(queryParams.someParam);
+
+  // ...
+};
+```
+
+To set up useQueryParams, you must wrap your top level component
+in QueryParamsProvider:
+
+```
+import { QueryParamsProvider } from '../UseQueryParams';
+
+const TopLevelComponent = () => {
+  return (
+    <QueryParamsProvider>
+      // ...
+    </QueryParamsProvider>
+  )
+};
+```
+
+*/
+
 import * as React from 'react';
 
 interface QueryParamsContextValue {
@@ -14,10 +69,12 @@ const getParamsFromURL = (): Record<string, any> => {
   return Object.fromEntries(url.searchParams.entries());
 };
 
-/* Wrap main component in this to provide context */
+// Wrap top level component in this to provide context
 export const QueryParamsProvider: React.FC<React.PropsWithChildren<any>> = (props) => {
   const [queryParams, setQueryParams] = React.useState<Record<string, any>>(getParamsFromURL);
 
+  // Set a single query parameter.
+  // If set to null, the parameter will be deleted.
   const setQueryParam = React.useCallback(
     (key: string, value: any) => {
       setQueryParams((p) => {
@@ -33,7 +90,7 @@ export const QueryParamsProvider: React.FC<React.PropsWithChildren<any>> = (prop
     [setQueryParams]
   );
 
-  // Update query params in URL
+  // Update query string in the URL
   React.useEffect(() => {
     const url = new URL(window.location.href);
     url.search = new URLSearchParams(queryParams).toString();
@@ -52,6 +109,14 @@ export const QueryParamsProvider: React.FC<React.PropsWithChildren<any>> = (prop
   );
 };
 
+// Import this to get and set query parameters.
+//
+// Usage:
+//   const { queryParams, setQueryParam } = useQueryParams();
+//
+// !! IMPORTANT !!
+//   You must wrap your top level component in QueryParamsProvider or
+//   useQueryParams will not work!!
 export const useQueryParams = (): QueryParamsContextValue => {
   const context = React.useContext(QueryParamsContext);
   if (context === undefined) {
